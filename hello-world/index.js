927746a59dfbd8ac1af143c76fac36891e21b212
DianaCarolina3 <dcaroli2015@gmail.com>
2021-06-14 12:48:45 -0500

    creando servidor con express js

---
+++ hello-world/index.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res, next) => {
+  res.send({ hello: 'world' })
+})
+
+const server = app.listen(8000, () => {
+  console.log(`Listening http://localhost:${server.address().port}`)
+})
