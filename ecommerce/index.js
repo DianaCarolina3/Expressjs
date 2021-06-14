924d51fbfaf8df852bfece3695c945f75bdcc005
DianaCarolina3 <dcaroli2015@gmail.com>
2021-06-14 17:54:37 -0500

    usando Pug como template engine

---
+++ ecommerce/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+//devuelve la ruta del directorio actual
+const path = require('path')
+const app = express()
+
+const productsRouter = require('./routes/products')
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
+app.use('/products', productsRouter)
+
+const server = app.listen(8000, () => {
+  console.log(`Listening http://localhost:${server.address().port}`)
+})
